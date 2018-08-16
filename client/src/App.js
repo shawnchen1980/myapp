58f4ee0003f19b1af5cca490eae4ee503dfc7426
DESKTOP-HAEDBP1\shawn <shawnchen1980@hotmail.com>
2018-08-16 23:40:47 +0800

    A demo showing when the life cycles including componentWillReceiveProps->shouldComponentUpdate->componentWillUpdate->render->componentDidUpdate will be triggered.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import MyCom from "./MyCom";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <MyCom />
-    <p className="App-intro">
-      To get started, edit
-      <code>src/App.js</code>
-      and save to reload.
-    </p>
-  </div>
-);
+class App extends Component {
+  state = { content: "hello world!", useless: "world hello!" };
+
+  render() {
+    const { content, useless } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <input
+          value={content}
+          onChange={event => this.setState({ content: event.target.value })}
+        />
+        <input
+          value={useless}
+          onChange={event => this.setState({ useless: event.target.value })}
+        />
+        <MyCom content={content} useless={useless} />
+        <p className="App-intro">
+          To get started, edit
+          <code>src/App.js</code>
+          and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
--- client/src/MyCom.js
+++ client/src/MyCom.js
@@ -1,3 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-export default () => <h1>hello world</h1>;
+class MyCom extends Component {
+  static propTypes = {
+    content: PropTypes.string,
+    useless: PropTypes.string
+  };
+
+  static defaultProps = {
+    content: "",
+    useless: ""
+  };
+
+  constructor(props) {
+    super(props);
+    console.log("Inside MyCom constructor", props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("Inside MyCom componentWillReceiveProps", nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const { content } = this.props;
+    console.log("Inside MyCom shouldComponentUpdate", nextProps, nextState);
+    return nextProps.content !== content;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log("Inside MyCom componentWillUpdate", nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log("Inside MyCom componentDidUpdate");
+  }
+
+  render() {
+    const { content, useless } = this.props;
+
+    return (
+      <div>
+        <p>{content}</p>
+        <p>{useless}</p>
+      </div>
+    );
+  }
+}
+export default MyCom;
